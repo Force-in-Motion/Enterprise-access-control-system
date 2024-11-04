303f402f02b1d0e0ea05e900c917b3f5c972325c
Force-in-Motion <canzdao@gmail.com>
2024-11-04 19:40:25 +0300

    feat project: create structure project

---
+++ src/service/data_service.py
@@ -0,0 +1,18 @@
+class DataService:
+
+    @staticmethod
+    def check_folder():
+        pass
+
+    @staticmethod
+    def check_file():
+        pass
+
+    @staticmethod
+    def read_data():
+        pass
+
+    @staticmethod
+    def write_data():
+        pass
+    
